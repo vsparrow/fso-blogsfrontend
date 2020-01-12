f33395b382f7882734b40ff3118b9d6ff907f4ea
vsparrow.com <khan@vsparrow.com>
2020-01-12 01:11:05 +0000

    Login component now getting back token from backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, {useState} from 'react';
+import blogService from './services/blogs'
 
 //add login form
-const Login = ({username, password, setUsername, setPassword}) => {
+const Login = ({username, password, user, setUsername, setPassword, setUser}) => {
 	// const handleChange = (event.target)=>
-	
+	const handleLogin = async (e)=>{
+		e.preventDefault()
+		console.log("logging in with", username, password)
+		try{
+			const user = await blogService.login({username, password})
+			setUser(user)
+			setUsername('')
+			setPassword('')
+			console.log(user)
+		}
+		catch(exception){
+				
+		}
+	}
 	return(
 		<div>
 			<h2>log in to application</h2>
-			<form>
+			<form onSubmit={handleLogin}>
 				<div>username <input type='text' name='username' value={username} onChange={e=>setUsername(e.target.value)}/></div>
 				<div>password <input type='text' name='password' value={password} onChange={e=>setPassword(e.target.value)}/></div>
+				<button type='submit'>login</button>
 			</form>
 		</div>
 	)
@@ -19,10 +34,12 @@ const Login = ({username, password, setUsername, setPassword}) => {
 const App = () => {
 	const [username, setUsername] = useState('')
 	const [password, setPassword] = useState('')
+	const [user, setUser] = useState('')
+	
 	console.log(`username:${username} || password:${password}`)
 	return (
 	<div className="App">
-	  <Login username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>
+	  <Login username={username} setUsername={setUsername} password={password} setPassword={setPassword} user={user} setUser={setUser} />
 	</div>
 	);
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 // const baseUrl = '/api/blogs'
-const baseUrl = 'korea-ide-api.run.goorm.io/api/blogs'
+const baseUrl = 'http://korea-ide-api.run.goorm.io/api/login'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const login = async credentials => {
+	console.log('credentials is:', credentials)
+	console.log('baseUrl is:', baseUrl)
+	
+	const response = await axios.post(baseUrl, credentials)
+	return response.data
+}
+
+export default { getAll, login }
