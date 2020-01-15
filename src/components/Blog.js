ee4ec7d4becf8d7604f7a2f3b74d26c1f5cb6b53
vsparrow.com <khan@vsparrow.com>
2020-01-15 17:47:34 +0000

    component Blog, update to show details on click

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 const Blog = ({ blog }) => {
+	
+	const [showDetails, setShowDetails] = useState(false)
+	const noDetails = {display: showDetails ? 'none' : ''}
+	const allDetails = {display: showDetails ? '' : 'none'}
+	
 	const blogStyle = {
 		paddingTop: 10,
-		paddingLeft: 2,
+		paddingLeft: 2,	
 		border: 'solid',
 		borderWidth: 1,
-		marginBottom: 5	
+		marginBottom: 5,
 	}		
-	
+	const handleClick = (e)=>{
+		setShowDetails(!showDetails)
+	}
 	return(
-		<div style={blogStyle} onClick={()=>{console.log("clicked")}}>
-			{blog.title} {blog.author}
+		<div>
+			<div style={Object.assign(noDetails,blogStyle )} onClick={handleClick}>
+				{blog.title} {blog.author}
+			</div>
+			<div style={Object.assign(allDetails,blogStyle )} onClick={handleClick}>
+				<div>{blog.title} {blog.author}</div>
+				<div>{blog.url}</div>
+				<div>{blog.likes} likes</div>
+			</div>
 		</div>
+
 	)
 }
 
