506151a65a841ed926101181c0886fdb47a19a78
vsparrow.com <khan@vsparrow.com>
2020-01-15 15:57:00 +0000

    update component Blog style

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,18 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: 'solid',
+		borderWidth: 1,
+		marginBottom: 5	
+	}		
+	
+	return(
+		<div style={blogStyle} onClick={()=>{console.log("clicked")}}>
+			{blog.title} {blog.author}
+		</div>
+	)
+}
 
 export default Blog
