1566c9bec6303635cd5c68c1b5c28db55dabf0cc
vsparrow.com <khan@vsparrow.com>
2020-01-18 00:16:10 +0000

    add tests to check if like button fires onClick

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import {prettyDOM} from '@testing-library/dom'
 import SimpleBlog from './SimpleBlog'
 
@@ -17,5 +17,14 @@ test('renders a blog', ()=>{
 	// console.log(prettyDOM(likes))
 })
 
-
+test('clicking the link button calls the event handler twice', ()=>{
+	const blog = {title: 'a test blog', author: 'Clark Kent', likes: 5}
+	const mockHandler = jest.fn()
+	const {getByText} = render(<SimpleBlog blog={blog} onClick={mockHandler}/>)
+	const button = getByText('like')
+	fireEvent.click(button)
+	fireEvent.click(button)
+	
+	expect(mockHandler.mock.calls.length).toBe(2)
+})
 
