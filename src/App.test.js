d332b90358fdaad5fbdb6b517ca7891c46ce68b9
vsparrow.com <khan@vsparrow.com>
2020-01-18 06:01:21 +0000

    add tests to see if blogs render when logged in

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,16 @@ import { render, waitForElement } from '@testing-library/react'
 jest.mock('./services/blogs')
 import App from './App'
 
+beforeEach(()=>{
+// const user = {
+//   username: 'misterfantastic',
+//   token: '1231231214',
+//   name: 'Reed Richards'
+// }
+
+// localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+})
+
 describe('<App />', ()=>{
 	test('if no user logged, blogs are not rendered', async ()=>{
 		const component = render(<App />)
@@ -16,4 +26,23 @@ describe('<App />', ()=>{
 		const blogs = component.container.querySelectorAll('.blog')
 		expect(blogs.length).toBe(0)
 	})	
+	
+	test('when a user is logged in blogs are rendered', async ()=>{
+		const user = {
+		  username: 'misterfantastic',
+		  token: '1231231214',
+		  name: 'Reed Richards'
+		}
+
+		localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+		const component = render(<App />)
+		component.rerender(<App />)
+		await waitForElement(()=>component.container.querySelector('.blog'))
+		// component.debug()
+		
+		expect(component.container).toHaveTextContent('HTML is easy')
+		
+		const blogs = component.container.querySelectorAll('.blog')
+		expect(blogs.length).toBe(3)
+	})
 })
