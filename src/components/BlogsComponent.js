aa8d29ce7efbfe67df78a308abff44c9468642c5
vsparrow.com <khan@vsparrow.com>
2020-01-29 02:13:09 +0000

    move NewBlogsForm to own file

--- src/components/BlogsComponent.js
+++ src/components/BlogsComponent.js
@@ -1,26 +1,8 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Message from './Message'
 import Blog from './Blog'
 import Toggable from './Toggable'
-// const Toggable = (props) => {
-// 	const [visible, setVisible] = useState(false)
-// 	const hideWhenVisible = { display: visible ? 'none' : ''}
-// 	const showWhenVisible = { display: visible ? '' : 'none'}
-	
-// 	const toggleVisibility = () => { setVisible(!visible)}
-	
-// 	return(
-// 		<div>
-// 			<div style={hideWhenVisible}>
-// 				<button onClick={toggleVisibility}>{props.buttonLabel}</button>
-// 			</div>
-// 			<div style={showWhenVisible}>
-// 				{props.children}
-// 				<button onClick={toggleVisibility}>cancel</button>
-// 			</div>
-// 		</div>
-// 	)
-// }
+import NewBlogsForm from './NewBlogsForm'
 
 const PostLoginMessage = ({user,setUser})=>{
 	return(<div><p>{user.name} logged in<LogoutButton setUser={setUser}/></p></div>)
@@ -29,41 +11,6 @@ const PostLoginMessage = ({user,setUser})=>{
 const LogoutButton = ({setUser})=>{
 	return <button onClick={()=>{window.localStorage.removeItem('loggedBlogappUser'); setUser(null);}}>logout</button>
 }
-
-const NewBlogsForm = ({user,blogs,setBlogs,message, setMessage}) => {
-	const [title,setTitle] = useState('')
-	const [author,setAuthor] = useState('')
-	const [url,setUrl] = useState('')	
-	
-	const createHandler = async (e) => { 
-		e.preventDefault()
-		console.log("hello")
-		const newBlogData = {title,url,author}
-		console.log(newBlogData)
-		//post the blog
-		try{
-			// const results = await blogService.postBlog(newBlogData, user.token) //******
-			// console.log("results are,", results)
-			// setBlogs(blogs.concat(results))
-			// setMessage(`a new blog ${results.title} by ${results.author} added`)
-		}catch(error){setMessage('error adding blog')}	
-		setTitle('')
-		setAuthor('')
-		setUrl('')
-	}
-	
-	return(
-		<div>
-			<h2>create new</h2>
-			<form>
-				<div>title:<input type='text' name='title' value={title} onChange={e=>setTitle(e.target.value)}/></div>
-				<div>author:<input type='text' name='author' value={author} onChange={e=>setAuthor(e.target.value)}/></div>
-				<div>url:<input type='text' name='url' value={url} onChange={e=>setUrl(e.target.value)}/></div>
-				<button onClick={createHandler}>create</button>
-			</form>
-		</div>
-	)		
-}  
 
 const BlogsComponent = ({blogs,setBlogs, user,setUser, message, setMessage})=>{
 	return(
---
+++ src/components/NewBlogsForm.js
@@ -0,0 +1,38 @@
+import React,{useState} from 'react'
+
+const NewBlogsForm = ({user,blogs,setBlogs,message, setMessage}) => {
+	const [title,setTitle] = useState('')
+	const [author,setAuthor] = useState('')
+	const [url,setUrl] = useState('')	
+	
+	const createHandler = async (e) => { 
+		e.preventDefault()
+		console.log("hello")
+		const newBlogData = {title,url,author}
+		console.log(newBlogData)
+		//post the blog
+		try{
+			// const results = await blogService.postBlog(newBlogData, user.token) //******
+			// console.log("results are,", results)
+			// setBlogs(blogs.concat(results))
+			// setMessage(`a new blog ${results.title} by ${results.author} added`)
+		}catch(error){setMessage('error adding blog')}	
+		setTitle('')
+		setAuthor('')
+		setUrl('')
+	}
+	
+	return(
+		<div>
+			<h2>create new</h2>
+			<form>
+				<div>title:<input type='text' name='title' value={title} onChange={e=>setTitle(e.target.value)}/></div>
+				<div>author:<input type='text' name='author' value={author} onChange={e=>setAuthor(e.target.value)}/></div>
+				<div>url:<input type='text' name='url' value={url} onChange={e=>setUrl(e.target.value)}/></div>
+				<button onClick={createHandler}>create</button>
+			</form>
+		</div>
+	)		
+}  
+
+export default NewBlogsForm
