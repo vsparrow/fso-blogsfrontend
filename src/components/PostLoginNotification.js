7f949bbd1a18f8bb1b6940b7cf7eedc52eaa4bc0
vsparrow.com <khan@vsparrow.com>
2020-01-31 01:34:28 +0000

    upon login, post login message rendered using redux

--- src/components/PostLoginNotification.js
+++ src/components/PostLoginNotification.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 const LogoutButton = ({setUser})=>{
 	return <button onClick={()=>{window.localStorage.removeItem('loggedBlogappUser'); setUser(null);}}>logout</button>
 }
 
-const PostLoginMessage = ({user,setUser})=>{
-	return(<div><p>{user.name} logged in<LogoutButton setUser={setUser}/></p></div>)
+const PostLoginMessage = (props)=>{
+	console.log(props)
+	return(<div>
+			<p>{props.user.name} logged in<LogoutButton setUser={props.setUser}/></p>
+			<p>ruser.name is {props.ruser ? props.ruser.name : ''}</p>
+		</div>)
 }
 
-export default PostLoginMessage
+const mapStateToProps = state => ({ruser: state.ruser})
+export default connect(mapStateToProps)(PostLoginMessage)
